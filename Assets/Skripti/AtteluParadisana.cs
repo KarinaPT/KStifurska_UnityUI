57a5516307c8d81fab808e4d746b79a0c2398124
Karina <kstifurska@gmail.com>
2021-03-16 17:11:07 +0200

    Vards, panel, atteli

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour
+{
+    public GameObject biksesC;
+    public GameObject jakaC;
+    public GameObject maskaC;
+    public GameObject zabakiC;
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+    void Update()
+    {
+        
+    }
+    public void biksesCAttelosana(bool vertiba)
+    {
+        biksesC.SetActive(vertiba);    
+    }
+    public void jakaCAttelosana(bool vertiba)
+    {
+        jakaC.SetActive(vertiba);
+    }
+    public void maskaCAttelosana(bool vertiba)
+    {
+        maskaC.SetActive(vertiba);
+    }
+    public void zabakiCAttelosana(bool vertiba)
+    {
+        zabakiC.SetActive(vertiba);
+    }
+    public void izkritosais(int skaitlis)
+    {
+        if(skaitlis == 0)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        }
+        if (skaitlis == 1)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        }
+    }
+}
---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+    public string teksts;
+    public GameObject ievadesLauks;
+    public GameObject tekstaAttelosana;
+
+    public void uzglabaTekstu()
+    {
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Supervaronis " + teksts.ToUpper() + "ir gadus vecs!";
+    }
+}
