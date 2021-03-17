6d8cb0bc48f63c96aafa94e58b58c401405cc32a
Karina <kstifurska@gmail.com>
2021-03-17 19:17:23 +0200

    Visi skaņas, elementu parvietosana

---
+++ Assets/Skripti/SoundAboutCaustic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SoundAboutCaustic : MonoBehaviour
+{
+    public AudioSource skanasAvots;
+    public AudioClip skanaKoAtskanot;
+    public void uzbiditsUzAttela()
+    {
+        skanasAvots.PlayOneShot(skanaKoAtskanot);
+    }
+    public void kursorsNoietNoAttela()
+    {
+        skanasAvots.Stop();
+    }
+}
